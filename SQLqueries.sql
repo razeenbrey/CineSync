0048cef13bf46b918b2208b6b0007454ba2a2b62
Razeen Brey <98354231+razeenbrey@users.noreply.github.com>
2024-07-09 15:38:58 +0200

    SQL Server | Update SQLqueries.sql

--- SQLqueries.sql
+++ SQLqueries.sql
@@ -1,29 +1,24 @@
 CREATE TABLE Users(
-  Uname VARCHAR(15) NOT NULL,
-  PRIMARY KEY(Uname)
+  uName VARCHAR(15) NOT NULL,
+  PRIMARY KEY(uName)
 );
 
 CREATE TABLE Series(
-  Sid VARCHAR(4) NOT NULL,
-  Sname VARCHAR(30) NOT NULL,
-  Producer VARCHAR(15) NOT NULL,
-  PRIMARY KEY(Sid)
+  sId VARCHAR(4) NOT NULL,
+  sName VARCHAR(30) NOT NULL,
+  producer VARCHAR(15) NOT NULL,
+  PRIMARY KEY(sId)
 );
 
 CREATE TABLE Episodes(
-  Eid VARCHAR(8) NOT NULL,
-  Ename VARCHAR(30) NOT NULL,
-  Status ENUM("Writing", "Written", "Filmed", "Editing", "Release Ready", "Completed") NOT NULL,
-  Synopsis VARCHAR(150) NOT NULL,
-  LeadPresenter VARCHAR(15) NOT NULL,
-  Writer VARCHAR(15) NOT NULL,
-  Guests VARCHAR(30),
-  PRIMARY KEY(Eid)
-);
-
--- use this for pdf storage
-CREATE TABLE PdfStorageTable
-(
-    Id INT PRIMARY KEY,
-    PdfData VARBINARY(7340032) -- 7 MB (7 * 1024 * 1024 bytes)
+  eId VARCHAR(8) NOT NULL,
+  eName VARCHAR(30) NOT NULL,
+  status ENUM("Writing", "Written", "Filmed", "Editing", "Release Ready", "Completed") NOT NULL,
+  synopsis VARCHAR(150) NOT NULL,
+  leadPresenter VARCHAR(15) NOT NULL,
+  writer VARCHAR(15) NOT NULL,
+  guests VARCHAR(30),
+  informationSheet VARBINARY(7340032) -- 7 MB (7 * 1024 * 1024 bytes)
+  productionSheet VARBINARY(7340032) -- 7 MB (7 * 1024 * 1024 bytes)
+  PRIMARY KEY(eId)
 );
