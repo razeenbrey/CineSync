3dfbef5cb5f0f11b984234c67fcfa4a5b6166695
Razeen Brey <98354231+razeenbrey@users.noreply.github.com>
2024-07-09 13:02:57 +0200

    SQL Server | Update SQLqueries.sql

--- SQLqueries.sql
+++ SQLqueries.sql
@@ -13,12 +13,17 @@ CREATE TABLE Series(
 CREATE TABLE Episodes(
   Eid VARCHAR(8) NOT NULL,
   Ename VARCHAR(30) NOT NULL,
-  -- Change to enum below
-  Status VARCHAR(30) NOT NULL,
+  Status ENUM("Writing", "Written", "Filmed", "Editing", "Release Ready", "Completed") NOT NULL,
   Synopsis VARCHAR(150) NOT NULL,
   LeadPresenter VARCHAR(15) NOT NULL,
   Writer VARCHAR(15) NOT NULL,
-  -- maybe make smaller
-  Guests VARCHAR(50) NOT NULL,
+  Guests VARCHAR(30),
   PRIMARY KEY(Eid)
+);
+
+-- use this for pdf storage
+CREATE TABLE PdfStorageTable
+(
+    Id INT PRIMARY KEY,
+    PdfData VARBINARY(7340032) -- 7 MB (7 * 1024 * 1024 bytes)
 );
