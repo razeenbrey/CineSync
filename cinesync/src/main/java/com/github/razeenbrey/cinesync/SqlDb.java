94b12bb1b89d5923ceeb567a1fdf29573a0627c3
Razeen Brey <98354231+razeenbrey@users.noreply.github.com>
2024-11-24 18:37:46 +0200

    SQL Standalone Class | Framework created.
    Created the framework for implementation of a standalone SQL class that manages DB Queries. Intened for a threaded environment. Currently not in service in the application but kept for possible future use.

--- cinesync/src/main/java/com/github/razeenbrey/cinesync/SqlDb.java
+++ cinesync/src/main/java/com/github/razeenbrey/cinesync/SqlDb.java
@@ -1,21 +1,43 @@
 package com.github.razeenbrey.cinesync;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SqlDb
-{   
-    Connection conn;
-
-    public boolean connectSql(String url, String username, String password)
-    {
-
-        try {
-            conn = DriverManager.getConnection(url, username, password);
-            return true;
-        } catch (SQLException e) {
-            return false;
-        }
-        
-    }
-
+ public class SqlDb implements Runnable
+ {  
+    public String url;
+    public String user;
+    private String passwd;
     
-}
+    public SqlDb(String url, String user, String passwd)
+    {
+        this.url = url;
+        this.user = user;
+        this.passwd = passwd;
+    }
+    
+    public void run()
+    {
+        // load driver
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found.");
+            e.printStackTrace();
+        }
+
+        // create connection
+        try (Connection conn = DriverManager.getConnection(url, user, passwd)){
+            // statements to execute go here
+            conn.close();
+
+        } catch (SQLException e1) {
+           e1.printStackTrace();
+        }
+    }
+ }
